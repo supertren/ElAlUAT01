8753d6bd34c5d0c777aafc07908a953e1daf81ea
supertren1 <supertren@gmail.com>
2022-12-15 21:41:41 +0100

    add files

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${var.proyect_name}-${var.environment}"
+  name = "${var.project_name}-${var.environment}"
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
