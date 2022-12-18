79e77de4113bd1e86e6fe71e55a0a230b0e624e4
supertren1 <supertren@gmail.com>
2022-12-18 14:36:06 +0100

    add files modify vars 2

--- vpc_am-dev.tf
+++ vpc_am-dev.tf
@@ -6,7 +6,7 @@ module "vpc" {
 
   azs             = ["${var.azs1}", "${var.azs2}", "${var.azs3}"]
   private_subnets = ["${var.Private-1a}", "${var.Private-1b}", "${var.Private-1c}"]
-  public_subnets  = ["${var.public_subnet1}", "${var.public_subnet2}", "${var.public_subnet3}"]
+  public_subnets  = ["${var.Public-1a}", "${var.Public-1b}", "${var.Public-1c}"]
 # intra_subnets =   ["10.0.51.0/24", "10.0.52.0/24", "10.0.53.0/24"]
   
   enable_nat_gateway = true
